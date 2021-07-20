22c5fcbfc08d8a697620a2892316ddd235beff09
ZaferGok <41114328+zzafergok@users.noreply.github.com>
2021-07-20 14:34:38 +0300

    add route campaigns page

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './view/pages/home/index.jsx'
 import Profile from './view/pages/profile/index.jsx'
 import OrderBasket from './view/pages/basket/index.jsx'
+import Campaigns from './view/pages/campaigns/index.jsx'
 
 export default function Routes() {
   return (
@@ -13,6 +14,7 @@ export default function Routes() {
           <Route path="/" component={Home} exact />
           <Route path="/profile" component={Profile} />
           <Route path="/order-basket" component={OrderBasket} />
+          <Route path="/campaigns" component={Campaigns} />
         </Switch>
       </Router>
     </>

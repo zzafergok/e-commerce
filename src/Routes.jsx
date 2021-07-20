9d8ac9fa023fe978c6005700f21e91f8e2f2968b
ZaferGok <41114328+zzafergok@users.noreply.github.com>
2021-07-20 19:39:17 +0300

    create new route

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -5,6 +5,9 @@ import Home from './view/pages/home/index.jsx'
 import Profile from './view/pages/profile/index.jsx'
 import OrderBasket from './view/pages/basket/index.jsx'
 import Campaigns from './view/pages/campaigns/index.jsx'
+import SolutionCenter from './view/pages/solution-center/index.jsx'
+import Login from './view/pages/authentication/login/index.jsx'
+import Register from './view/pages/authentication/register/index.jsx'
 
 export default function Routes() {
   return (
@@ -15,6 +18,9 @@ export default function Routes() {
           <Route path="/profile" component={Profile} />
           <Route path="/order-basket" component={OrderBasket} />
           <Route path="/campaigns" component={Campaigns} />
+          <Route path="/solution-center" component={SolutionCenter} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
         </Switch>
       </Router>
     </>

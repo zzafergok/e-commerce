a65fee578ec7d55bb15993ca29a5fb9d19cdd0f2
ZaferGok <41114328+zzafergok@users.noreply.github.com>
2021-07-20 19:40:38 +0300

    update refresh show loading bar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect, Suspense } from 'react'
 import 'antd/dist/antd.css'
 import './App.css'
+import RotateLoader from 'react-spinners/RotateLoader'
 
-import Routes from './Routes.jsx'
+const Routes = React.lazy(() => import('./Routes.jsx'))
 
 function App() {
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 4000)
+  }, [])
   return (
-    <>
-      <Routes />
-    </>
+    <Suspense
+      fallback={
+        <RotateLoader
+          color={'#4A90E2'}
+          loading={loading}
+          size={15}
+          margin={2}
+        />
+      }
+    >
+      <>
+        <Routes />
+      </>
+    </Suspense>
   )
 }
 
